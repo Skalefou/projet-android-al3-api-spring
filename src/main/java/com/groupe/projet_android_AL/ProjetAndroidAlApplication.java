208e4662e3675c32ae12b94e54506829d53327bb
Skalefou <skamsagar@gmail.com>
2025-10-24 16:24:20 +0200

    Initial commit

---
+++ src/main/java/com/groupe/projet_android_AL/ProjetAndroidAlApplication.java
@@ -0,0 +1,13 @@
+package com.groupe.projet_android_AL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAndroidAlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAndroidAlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/groupe/projet_android_AL/ProjetAndroidAlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe.projet_android_AL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAndroidAlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
